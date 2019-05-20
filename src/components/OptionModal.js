2bccfbf7832ac5e8ba83864e956d70b7e1a3c587
Van De Castro <vandecastro95@gmail.com>
2019-05-20 09:35:23 -0500

    100%

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,11 +6,14 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleOkay}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="Modal"
         >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
+            <h3 className="Modal__title">Selected Option</h3>
+            {props.selectedOption && <p className="Modal__body">{props.selectedOption}</p>}
 
             <button
+            className="button"
             onClick={props.handleOkay}
             >Okay</button>
         </Modal>
